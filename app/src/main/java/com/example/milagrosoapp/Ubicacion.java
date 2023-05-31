2e79bf9cf10baab1cd7f2d940190eab46102bde0
Daniel Caceres <dcaceres@servex-us.com>
2023-05-31 11:09:40 -0500

    Arreglo en mapa

--- app/src/main/java/com/example/milagrosoapp/Ubicacion.java
+++ app/src/main/java/com/example/milagrosoapp/Ubicacion.java
@@ -3,33 +3,45 @@ package com.example.milagrosoapp;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-
-import android.os.Bundle;
-import android.widget.Toast;
-
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.location.FusedLocationProviderClient;
 import com.example.milagrosoapp.databinding.ActivityUbicacionBinding;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+/*
+import com.google.android.gms.place.api.Places;
+import com.google.android.gms.places.api.model.Place;
+import com.google.android.gms.places.api.net.FindPlaceRequest;
+import com.google.android.gms.places.api.net.FindPlaceResponse;
+import com.google.android.gms.places.api.net.PlacesClient;
+
+ */
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Ubicacion extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityUbicacionBinding binding;
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +60,7 @@ public class Ubicacion extends FragmentActivity implements OnMapReadyCallback {
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -58,15 +69,14 @@ public class Ubicacion extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        // coordenadas de Bogotá
+        LatLng bogota = new LatLng(4.6097, -74.0817);
 
+        // Mueve la cámara a la ubicación de Bogotá con un nivel de zoom
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bogota, 12f));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
             return;
         }
         mMap.setMyLocationEnabled(true);
@@ -76,5 +86,6 @@ public class Ubicacion extends FragmentActivity implements OnMapReadyCallback {
         LatLng colombia = new LatLng(4.7009744, -74.1708576);
         mMap.addMarker(new MarkerOptions().position(colombia).title("Ubicación actual"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(colombia));
+
     }
 }
